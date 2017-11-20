a7fff775f26810b3ddf5c2fe019fb545ce18194c
Antti Simonen <antti.simonen@gofore.com>
2017-11-20 15:21:23 +0200

    Changed API endpoint to accept raw requests without content-type header

--- Uspator/Controllers/BotController.cs
+++ Uspator/Controllers/BotController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Uspator.Model;
@@ -10,10 +13,19 @@ namespace Uspator.Controllers
     public class BotController : Controller
     {
         [HttpPost]
-        public JsonResult Post([FromForm] ServerRequest value)
+        public async Task<JsonResult> Post()
         {
-            Console.WriteLine(JsonConvert.SerializeObject(value));
-
+            // ASP.NET Core does not allow us to get raw request body in any meaningful way
+            // and since BattleCube Server doesn't send us Content-Type 'application/json' header
+            // we need to read raw request body and then deserialize it into our object type
+            string requestBody;
+            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {  
+                requestBody = await reader.ReadToEndAsync();
+            }
+            var request = JsonConvert.DeserializeObject<ServerRequest>(requestBody);
+            
+            // Remember to send a list of tasks, but no more than what the request specifies
             var tasks = new List<TaskBase> {new NoopTask()};
             
             return new JsonResult(tasks);
